b71d7f8fb434c9958162ac5980cff0383915edce
Eli Freid <eli.freid@gmail.com>
2021-07-26 21:16:00 +0300

    added contact and button, and js

---
+++ resources/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('btn').addEventListener('click', function(){
+        document.getElementById("contact").style.display = 'flex';
+    })
+})
